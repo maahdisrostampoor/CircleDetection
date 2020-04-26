d8e3e55a4ec15d3352374b60a8a1c9ccecb0a367
rmahdiss <60295025+rmahdiss@users.noreply.github.com>
2020-04-26 13:14:34 +0430

    Update CircleDetection.py

--- CircleDetection.py
+++ CircleDetection.py
@@ -9,10 +9,12 @@ img_s = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
                            param1=200, param2=30, minRadius=0, maxRadius=150)
 
-if circles is not None:
+if circles is not None:#if there is a circle
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
+        #Circle's centers
         cv2.circle(img_s, (i[0], i[1]), 1, (0, 0, 255), 2)
+        #Circle's Outers
         cv2.circle(img_s, (i[0], i[1]), i[2], (0, 255, 0), 2)
 #cropping the circles out of the image
 masking = np.full((img.shape[0], img.shape[1]), 0, dtype=np.uint8)
